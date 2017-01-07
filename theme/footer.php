a02b0718efc686a9d8f9e67b3a94ddfc1e64a784
Matt Urban <email@matturban.com>
2017-01-07 09:25:22 -0800

    removes menu and social links from footer

--- theme/footer.php
+++ theme/footer.php
@@ -1,9 +1,5 @@
-<div id="footer"><hr /><?php get_template_part( 'menu' ); ?>
+<div id="footer"><hr />
 	&copy; <?php echo date('Y'); ?> MEH
-      	<div id="social">
-      		<a href="https://twitter.com/MichaelEHayden" target="_blank"><img src="/wp-content/uploads/2016/12/twitter-logo.png" /></a>
-        	<a href="https://www.facebook.com/michael.e.hayden.1" target="_blank"><img src="/wp-content/uploads/2016/12/facebook-logo.png" /></a>
-      	</div>
 </div>
 </body>
 </html>
