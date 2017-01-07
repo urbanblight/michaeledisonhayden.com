a2807144e4135da15ee3e4541e78db1cb0978589
Matt Urban <email@matturban.com>
2017-01-07 09:26:51 -0800

    moves social links to menu
    * and re-format/factor

--- theme/menu.php
+++ theme/menu.php
@@ -1,23 +1,35 @@
 <div id="menu">
+
 <?php
-$args=array(
-  'sort_order' => 'ASC',
-	'sort_column' => 'post_title'
-  );
-$pages=get_pages($args);
-  foreach($pages as $page) {
-    echo '<a href="'.get_page_link( $page->ID ). '">' . strtoupper ($page->post_title) . '</a> ';
-   }
-$args=array(
-  'orderby' => 'name',
-  'order' => 'ASC',
-  'depth' => 1
-  );
+
+$page_args = array('sort_order' => 'ASC',
+	        'sort_column' => 'post_title'
+            );
+
+$pages=get_pages($page_args);
+    foreach($pages as $page) {
+        echo '<span class="menu_item"><a href="'.get_page_link( $page->ID )
+             .'">'.strtoupper($page->post_title).'</a></span>';
+    }
+
+$cat_args=array('orderby' => 'name',
+                'order' => 'ASC',
+                'depth' => 1
+                );
+
 $categories=get_categories($args);
-  foreach($categories as $category) {
-	if ($category->parent==0) {
-    echo '<a href=" '.get_category_link( $category->term_id ). '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" ' . '>' . strtoupper ($category->name) . '</a> ';
+
+foreach($categories as $category) {
+    if ($category->parent==0) {
+        echo '<span class="menu_item"><a href=" '
+             .get_category_link( $category->term_id ).'" title="'
+             .sprintf( __( "View all posts in %s" ), $category->name )
+             . '" ' . '>' . strtoupper ($category->name) . '</a></span>';
 	}
    }
 ?>
+    <div id="social">
+    	<a href="https://twitter.com/MichaelEHayden" target="_blank"><img src="/wp-content/uploads/2016/12/twitter-logo.png" /></a>
+    	<a href="https://www.facebook.com/michael.e.hayden.1" target="_blank"><img src="/wp-content/uploads/2016/12/facebook-logo.png" /></a>
+    </div>
 </div>
