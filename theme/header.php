d5e7e2be0a1f58389948658f24b1d9cb8b6cb5c8
Matt Urban <email@matturban.com>
2017-01-16 19:04:37 -0800

    initial draft of responsive menu

--- theme/header.php
+++ theme/header.php
@@ -1,9 +1,14 @@
 <!DOCTYPE HTML>
 <head>
 <title><?php bloginfo('name')?></title>
-<link  href="http://fonts.googleapis.com/css?family=Open+Sans:regular,italic" rel="stylesheet">
+<link  href="http://fonts.googleapis.com/css?family=Open+Sans:regular,italic"
+	rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
 <link  href="<?php bloginfo('stylesheet_url') ?>" rel="stylesheet">
-<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name') ?> RSS Feed" href="<?php bloginfo('rss2_url')?>">
+<link rel="alternate" type="application/rss+xml"
+	title="<?php bloginfo('name') ?> RSS Feed"
+	href="<?php bloginfo('rss2_url')?>">
 <script src="<?php bloginfo('template_url')?>"></script>
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 <?php wp_head()?>
--- theme/menu.php
+++ theme/menu.php
@@ -1,31 +1,34 @@
-<div id="menu">
+<ul class="menu" id="theMenu">
+	<li class="icon">
+		<a href="javascript:void(0);" onclick="toggleMenu()">
+			<i class="material-icons">reorder</i>
+		</a>
+	</li>
 
 <?php
 
-$page_args = array('sort_order' => 'ASC',
-	        'sort_column' => 'post_title'
-            );
-
+$page_args = array('sort_order' => 'ASC', 'sort_column' => 'post_title');
 $pages=get_pages($page_args);
-    foreach($pages as $page) {
-        echo '<span class="menu_item"><a href="'.get_page_link( $page->ID )
-             .'">'.strtoupper($page->post_title).'</a></span>';
-    }
+foreach($pages as $page) {
+	echo '<span class="menu_item">
+			<a href="'.get_page_link( $page->ID ).'">'
+				.strtoupper($page->post_title)
+			.'</a>
+		</span>';
+}
 
-$cat_args=array('orderby' => 'name',
-                'order' => 'ASC',
-                'depth' => 1
-                );
-
+$cat_args=array('orderby' => 'name', 'order' => 'ASC', 'depth' => 1);
 $categories=get_categories($args);
-
 foreach($categories as $category) {
-    if ($category->parent==0) {
-        echo '<span class="menu_item"><a href="javascript:void(0)" '
-			 .'onclick="$(\'#viewing_area\').load(\''
-			 	.get_category_link( $category->term_id ).' #page_content\');"'
-             .'" title="'.sprintf(__( "View all posts in %s" ), $category->name)
-             . '">'.strtoupper($category->name).'</a></span>';
+	if ($category->parent==0) {
+		echo '<span class="menu_item">
+				<a href="javascript:void(0)" '
+					.'onclick="$(\'#viewing_area\').load(\''
+					.get_category_link($category->term_id).' #page_content\');"'
+             		.'" title="'.sprintf(__("View all posts in %s"), $category->name)
+             		.'">'.strtoupper($category->name)
+				.'</a>
+			  </span>';
 	}
 }
 ?>
@@ -34,4 +37,16 @@ foreach($categories as $category) {
 			<img src="/wp-content/uploads/2016/12/twitter-logo.png" />
 		</a>
     </div>
-</div>
+</ul>
+<script>
+/* Toggle between adding and removing the "responsive" class to menu when the
+	user clicks on the icon */
+function toggleMenu() {
+	var el = document.getElementById("menu");
+	if (el.className === "menu") {
+		el.className += " responsive";
+	} else {
+		el.className = "menu";
+    }
+}
+</script>
